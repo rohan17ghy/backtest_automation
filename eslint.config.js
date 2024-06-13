8bf70eb5bb214e112fa5eba8ee66751d7c367093
Debopriya_Bhattachar <rohan17ghy@gmail.com>
2024-06-13 20:04:53 +0530

    Moving to ES6 module in typescript with tsx and setting up eslint.config.js
    Moving to ES6 module in typescript with tsx and setting up eslint.config.js

---
+++ eslint.config.js
@@ -0,0 +1,37 @@
+//This file is expected to be a JS file
+// eslint.config.js
+import eslintPluginTypeScript from '@typescript-eslint/eslint-plugin';
+import { parseForESLint } from '@typescript-eslint/parser';
+import eslintPluginImport from 'eslint-plugin-import';
+
+export default [
+  {
+    ignores: ['node_modules/**'],
+  },
+  {
+    files: ['**/*.ts', '**/*.tsx'], // Include TypeScript files
+    languageOptions: {
+      parser: {
+        parseForESLint,
+        // You can add parserOptions here if needed
+        parserOptions: {
+          ecmaVersion: 2020,
+          sourceType: 'module',
+          project: './tsconfig.json', // Point to your tsconfig.json
+        },
+      },
+    },
+    plugins: {
+      '@typescript-eslint': eslintPluginTypeScript,
+      'import': eslintPluginImport,
+    },
+    rules: {
+      'no-unused-vars': 'off',
+      '@typescript-eslint/no-unused-vars': 'warn',
+      '@typescript-eslint/explicit-function-return-type': 'off',
+      '@typescript-eslint/no-explicit-any': 'warn',
+      'import/no-cycle': 'error',
+      // Add more rules as needed
+    },
+  },
+];
